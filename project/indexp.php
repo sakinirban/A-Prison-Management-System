14e826b5455bc127fda47837c4a7f8c7a63f4568
AfridiHassan00 <120330404+AfridiHassan00@users.noreply.github.com>
2022-12-12 00:01:23 +0600

    Add files via upload

---
+++ project/indexp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Prisoner Dashboard</title>
+  </head>
+  <body>
+<center><h1>Prisoner Dashboard</h1></center>
+
+<div class="#">
+  <a href="#">Update Profile</a>
+</div>
+  </body>
+</html>
---
+++ project/psignup.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+<link rel="stylesheet" href="registration.css">
+    <title>Prisoner Registration</title>
+  </head>
+  <body>
+<center>
+  <div class="container">
+
+
+<br>
+<br>
+<form class="box" action="dsignup.php" method="post">
+  <div class="user">
+    <div class="input-box">
+      <span class="details">Id </span>
+<input type="text" name="name" placeholder="Unique Id">
+    </div>
+    <div class="input-box">
+      <span class="details">First Name </span>
+<input type="text" name="name" placeholder="Your Name">
+    </div>
+    <div class="input-box">
+      <span class="details">Last Name </span>
+<input type="text" name="name" placeholder="Your Name">
+    </div>
+
+    <div class="input-box">
+      <span class="details">Covicted For </span>
+<input type="text" name="name" placeholder="Covicted For">
+    </div>
+
+    <div class="input-box">
+      <span class="details"> Password</span>
+      <input type="password" name="password" placeholder="Password">
+    </div>
+
+    </div>
+
+    <div class="button">
+    <input type="submit" name="registration" value="Registration">
+    </div>
+</div>
+</form>
+<br>
+<a href="indexj.html">Home Page</a>
+  </div>
+</center>
+  </body>
+</html>
